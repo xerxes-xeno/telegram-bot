a9c9d795227c626f476dbe631012175308ca84db
xerxes-xeno <karnsingh1800@gmail.com>
2026-09-03 17:54:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -844,8 +844,8 @@ async def helpdex(update, context):
                 callback_data="dex_pokeballs"
             ),
             InlineKeyboardButton(
-                "⚔️ 𝐌𝐨𝐯𝐞𝐬",
-                callback_data="dex_moves"
+                "⚔️ 𝐌𝐨𝐯𝐞 𝐃𝐚𝐭𝐚",
+                callback_data="dex_move"
             )
         ]
     ]
